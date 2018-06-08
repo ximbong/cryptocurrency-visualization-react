2b8175b0584422d66ca9762057e368c7197b643f
Hai Truong <haitruong181096@gmail.com>
2018-06-08 16:16:36 +0300

    Set up Header and Input states

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import Input from "../Input";
 import Button from "../Button";
 
-const Header = () => {
-  return (
-    <div className="buttons">
-      <Input />
-      <div className="sort-buttons">
-        <Button id="name" />
-        <Button id="price" />
-        <Button id="name" />
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ""
+    };
+  }
+  handleChange = value => {
+    this.setState({
+      input: value
+    });
+  };
+  render() {
+    return (
+      <div className="buttons">
+        <Input onChange={this.handleChange} />
+        <div className="sort-buttons">
+          <Button id="name" />
+          <Button id="price" />
+          <Button id="name" />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Header;
--- src/Components/Input/index.js
+++ src/Components/Input/index.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Input = () => {
-  return (
-    <div className="input">
-      <input type="text" />
-    </div>
-  );
-};
+class Input extends Component {
+  handleChange = event => {
+    const value = event.target.value;
+    this.props.onChange(value);
+  };
+  render() {
+    return (
+      <div className="input">
+        <input type="text" onInput={this.handleChange} />
+      </div>
+    );
+  }
+}
 
 export default Input;
