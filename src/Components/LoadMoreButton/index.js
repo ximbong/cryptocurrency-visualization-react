03af844a6b4efa0daa02412f8b16c0e5cedceb79
Hai Truong <haitruong181096@gmail.com>
2018-06-10 16:37:04 +0300

    Add load more button and modify some CSS

---
+++ src/Components/LoadMoreButton/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoadMoreButton = props => {
+  return <div className="load-more">Load more</div>;
+};
+
+export default LoadMoreButton;
--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
 
+import LoadMoreButton from "../LoadMoreButton";
 import Coins from "../Coins";
 
-const Main = ({ data }) => {
-  const listCoins = data.map((e, i) => <Coins data={e} key={i} />);
-  return <div className="container">{listCoins}</div>;
-};
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sort: "rank",
+      lastIndex: 100
+    };
+  }
+  render() {
+    const renderData = this.props.data.slice(0, this.state.lastIndex);
+    const listCoins = renderData.map((e, i) => <Coins data={e} key={i} />);
+    return (
+      <div className="container">
+        {listCoins}
+        <LoadMoreButton />
+      </div>
+    );
+  }
+}
 
 export default Main;
