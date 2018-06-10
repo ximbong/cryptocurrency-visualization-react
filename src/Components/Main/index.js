eb39498c29322dfcff31e6c73e601a3603abd01e
ximbong91023 <ximbong91023@users.noreply.github.com>
2018-06-10 19:00:19 +0300

    Add sort and filter. Also include conditional render for LoadMoreButton.

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -4,20 +4,62 @@ import LoadMoreButton from "../LoadMoreButton";
 import Coins from "../Coins";
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sort: "rank",
-      lastIndex: 100
-    };
-  }
+  sort = (array, category) => {
+    let renderData = [];
+
+    switch (category) {
+      case "price": {
+        renderData = array.sort(function(a, b) {
+          return b.quotes.USD.price - a.quotes.USD.price;
+        });
+        break;
+      }
+      case "name": {
+        renderData = array.sort(function(a, b) {
+          const nameA = a.name.toUpperCase();
+          const nameB = b.name.toUpperCase();
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+        });
+        break;
+      }
+      default: {
+        renderData = array.sort(function(a, b) {
+          return a.rank - b.rank;
+        });
+      }
+    }
+
+    return renderData;
+  };
+
+  filter = (array, value) => {
+    const filterArray = array.filter(function(element) {
+      return element.name.toLowerCase().includes(value.toLowerCase());
+    });
+    return filterArray;
+  };
+
   render() {
-    const renderData = this.props.data.slice(0, this.state.lastIndex);
-    const listCoins = renderData.map((e, i) => <Coins data={e} key={i} />);
+    const { dataArray, inputValue, sortType } = this.props;
+
+    const renderData = this.sort(dataArray, sortType);
+    const filterArray = this.filter(renderData, inputValue);
+
+    const listCoins = filterArray
+      .slice(0, this.props.lastIndex)
+      .map((e, i) => <Coins data={e} key={i} />);
+
     return (
       <div className="container">
         {listCoins}
-        <LoadMoreButton />
+        {filterArray.length > this.props.lastIndex && (
+          <LoadMoreButton increaseIndex={this.props.increaseIndex} />
+        )}
       </div>
     );
   }
