0def0deed74f7cf84a2360a308548ffd90d9b053
ximbong91023 <ximbong91023@users.noreply.github.com>
2018-06-10 18:58:45 +0300

    Add resetIndex and handleInput.

--- src/Components/Input/index.js
+++ src/Components/Input/index.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Input extends Component {
-  handleChange = event => {
-    const value = event.target.value;
-    this.props.onChange(value);
-  };
-  render() {
-    return (
-      <div className="input">
-        <input type="text" onInput={this.handleChange} />
-      </div>
-    );
-  }
-}
+const Input = ({ resetIndex, handleInput }) => {
+  return (
+    <div className="input">
+      <input
+        type="text"
+        onInput={event => {
+          resetIndex();
+          handleInput(event);
+        }}
+      />
+    </div>
+  );
+};
 
 export default Input;
