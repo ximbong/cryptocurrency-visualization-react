d5418ffec9e30a49318faa8f5696142c5c2fee1a
Hai Truong <haitruong181096@gmail.com>
2018-06-11 12:46:34 +0300

    Add Loader

---
+++ src/Components/Loader/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="dot dot-1" />
+      <div className="dot dot-2" />
+      <div className="dot dot-3" />
+    </div>
+  );
+};
+
+export default Loader;
