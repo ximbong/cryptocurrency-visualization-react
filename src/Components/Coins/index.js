bc45f92fbaf8c93226cb2e53d1f8c57f80dec93c
ximbong91023 <ximbong91023@users.noreply.github.com>
2018-06-14 02:28:11 +0300

    Change into PureComponent

--- src/Components/Coins/index.js
+++ src/Components/Coins/index.js
@@ -1,38 +1,40 @@
-import React from "react";
+import React, { PureComponent } from "react";
 
 import Percentage from "../Percentage";
 
-const Coins = props => {
-  const baseImgUrl = "https://s2.coinmarketcap.com/static/img/coins/32x32/";
-  const {
-    id,
-    name,
-    rank,
-    quotes: {
-      USD: { percent_change_1h, percent_change_7d, percent_change_24h, price }
-    }
-  } = props.data;
-  return (
-    <div className="coin">
-      <div className="coin-name" id={id}>
-        <img src={baseImgUrl + id + ".png"} alt={name + " icon"} />
-        {name}
+class Coins extends PureComponent {
+  render() {
+    const baseImgUrl = "https://s2.coinmarketcap.com/static/img/coins/32x32/";
+    const {
+      id,
+      name,
+      rank,
+      quotes: {
+        USD: { percent_change_1h, percent_change_7d, percent_change_24h, price }
+      }
+    } = this.props.data;
+    return (
+      <div className="coin">
+        <div className="coin-name" id={id}>
+          <img src={baseImgUrl + id + ".png"} alt={name + " icon"} />
+          {name}
+        </div>
+        <div className="rank">
+          <span>Rank: </span>
+          <span>{rank}</span>
+        </div>
+        <div className="price">
+          <span>Price: </span>
+          <span>{price}$</span>
+        </div>
+        <div className="price_change">
+          <Percentage percent={percent_change_1h} name="hour" />
+          <Percentage percent={percent_change_24h} name="day" />
+          <Percentage percent={percent_change_7d} name="week" />
+        </div>
       </div>
-      <div className="rank">
-        <span>Rank: </span>
-        <span>{rank}</span>
-      </div>
-      <div className="price">
-        <span>Price: </span>
-        <span>{price}$</span>
-      </div>
-      <div className="price_change">
-        <Percentage percent={percent_change_1h} name="hour" />
-        <Percentage percent={percent_change_24h} name="day" />
-        <Percentage percent={percent_change_7d} name="week" />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Coins;
